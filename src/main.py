55280295844cc42d62369f70349d715d38f14bd1
TuurMellaerts <tuur.mellaerts1@hotmail.be>
2023-12-28 21:55:13 +0100

    Added tkinter window

--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
+# This file implements a temperature converter application.
+# Designed by Tuur Mellaerts
+# Version: v0.1
+
+# General imports
+import tkinter as tk
+
 def main():
     print("Hello World!")
+    root = tk.Tk()
+
+    root.title("Temperature Converter")
+
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
