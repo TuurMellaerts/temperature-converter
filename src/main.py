f37456d77eea7f2fe374bad528b4f48c088cdb41
TuurMellaerts <tuur.mellaerts1@hotmail.be>
2024-01-05 12:58:23 +0100

    Added conversion between Fahrenheit and Celsius

--- src/main.py
+++ src/main.py
@@ -1,10 +1,22 @@
 # This file implements a temperature converter application.
 # Designed by Tuur Mellaerts
-# Version: v0.1
+# Version: v0.2
+# Date: 05/01/2024
 
 # General imports
 import tkinter as tk
 
+# Function to convert Celsius to Fahrenheit
+def convertCtoF(celsius):
+    fahrenheit = celsius * (9/5) + 32
+    return fahrenheit
+
+# Function to convert Fahrenheit to Celsius
+def convertFtoC(fahrenheit):
+    celsius = (fahrenheit - 32) * (5/9)
+    return celsius
+
+# Main function
 def main():
     root = tk.Tk()
 
@@ -18,5 +30,6 @@ def main():
 
     root.mainloop()
 
+# Start-up function
 if __name__ == "__main__":
     main()
